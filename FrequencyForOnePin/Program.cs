83b60c006536f327025f0864daa19f36605fe45a
Kota <kota@pop-os.localdomain>
2021-01-17 11:08:34 -0500

    Add project file

---
+++ FrequencyForOnePin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Device;
+using System;
+using System.Device.Gpio;
+using System.Diagnostics;
+
+namespace FrequencyForOnePin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputPinNum = 6;
+            var gpio = new GpioController();
+            gpio.OpenPin(inputPinNum, PinMode.Input);
+
+            gpio.Read(inputPinNum); // Warmup
+
+            var stopWatch = Stopwatch.StartNew();
+            var cycleCounter = 0;
+
+            while (stopWatch.Elapsed.TotalSeconds < 5)
+            {
+                gpio.Read(inputPinNum);
+                ++cycleCounter;
+            }
+
+            Console.WriteLine("Read Frequency for input pin #" + inputPinNum.ToString() +" : "
+                  + cycleCounter / stopWatch.Elapsed.TotalSeconds + "Hz");
+        }
+    }
+}
